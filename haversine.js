c970568d457e22e74f3c43666f2a147133e09d03
Jake Thakur <34597857+jakethakur22@users.noreply.github.com>
2018-01-29 18:31:54 +0000

    Update haversine.js
    various fixes and comments

--- haversine.js
+++ haversine.js
@@ -1,27 +1,29 @@
 //an application of the Haversine formula, in order to calculate the distance between two longitude and latitudes
 //https://www.movable-type.co.uk/scripts/latlong.html
 
+//returns answer in metres
+
 function haversine(lat1, long1, lat2, long2) {
-  var lat1 = lat1; //first latitude
-  var long1 = long1; //first longitude
-
-  var lat2 = lat2; //second latitude
-  var long2 = long2; //second longitude
-
   var R = 6371e3; // radius of the Earth, in metres
 
-  var φ1 = lat1.toRadians();
-  var φ2 = lat2.toRadians();
+  var lat1Radians = toRadians(lat1);
+  var lat2Radians = toRadians(lat2);
 
-  var Δφ = (lat2-lat1).toRadians();
-  var Δλ = (long2-long1).toRadians();
+  var deltaLat = toRadians(lat2-lat1);
+  var deltaLong = toRadians(long2-long1);
 
-  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+          Math.cos(lat1Radians) * Math.cos(lat2Radians) *
+          Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
   var d = R * c;
+  
+  return d;
+}
+
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
 }
 
 console.log(haversine(36.12, -86.67, 33.94, -118.40));
