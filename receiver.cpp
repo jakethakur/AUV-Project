42b477f8be814876da40be5bc6aa4b554a13b1c1
alexgillespie02 <35962042+alexgillespie02@users.noreply.github.com>
2018-05-07 14:53:25 +0100

    Create receiver.cpp

---
+++ receiver.cpp
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    int long;
+    int lat;
+    radio.read(&long, sizeof(long));
+    Serial.println(long);
+    radio.read(&lat, sizeof(lat));
+  }
+}
