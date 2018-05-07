165b21e5092b80f26ee5d07b2a1bc596e362f170
alexgillespie02 <35962042+alexgillespie02@users.noreply.github.com>
2018-05-07 15:15:32 +0100

    Create sender.cpp

---
+++ sender.cpp
@@ -0,0 +1,18 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  const int long;
+  const int lat;
+  radio.write(&long, sizeof(long));
+  radio.write(&lat, sizeof(lat));
+  delay(1000);
+}
