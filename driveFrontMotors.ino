abf899293e5e1060f6095e051958a7f8e0286cde
Louis Joseph Bishop <louis.bishop@live.co.uk>
2018-06-05 13:40:08 +0100

    Front and Back Drive
    Will add steering when figured out.

---
+++ driveFrontMotors.ino
@@ -0,0 +1,9 @@
+void setup() {
+  pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(12, HIGH);
+
+}
---
+++ driveRearMotors.ino
@@ -0,0 +1,9 @@
+void setup() {
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+
+}
