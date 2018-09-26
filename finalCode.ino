d93254f6e0bdbc42d21cfc5d90b4a6a0ed7b531f
Jake Thakur <34597857+jakethakur22@users.noreply.github.com>
2018-09-26 14:51:50 +0100

    final code from yesterday
    magnetometer

--- finalCode.ino
+++ finalCode.ino
@@ -7,9 +7,14 @@
 
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
+#include <SparkFun_MAG3110.h>
 
 SoftwareSerial mySerial(10, 11);
 TinyGPS gps;
+
+MAG3110 mag = MAG3110(); //Instantiate MAG3110
+
+
 
 //51.36513, -0.18960
 
@@ -18,47 +23,35 @@ TinyGPS gps;
 
 void setup()  
 {
-  // make the car face north!
-
-  
-  
-  // Oploen serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  delay(1000);
-  Serial.println("uBlox Neo 6M");
-  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println("by Mikal Hart");
-  Serial.println();
-  Serial.print("Sizeof(gpsobject) = "); 
-  Serial.println(sizeof(TinyGPS));
-  Serial.println(); 
   
   pinMode(13, OUTPUT); // forward
   pinMode(12, OUTPUT); // right
   
   delay(1000);
+
+  mag.initialize(); // Initializes the mag sensor
+  mag.start();      // Puts the sensor in active mode
 }
 
+// get latitude
 float latitude(TinyGPS &gps) {
   float flat, flon;
   gps.f_get_position(&flat, &flon);
   return flat;
 }
 
+// get lognitude
 float longitude(TinyGPS &gps) {
   float flat, flon;
   gps.f_get_position(&flat, &flon);
   return flon;
 }
 
-
-
-
-
-
-// unused
+// debug function: get current lat and long
 void gpsdump(TinyGPS &gps)
 {
   float flat, flon;
@@ -66,14 +59,10 @@ void gpsdump(TinyGPS &gps)
   Serial.print("Lat/Long(float): "); printFloat(flat, 5); Serial.print(", "); printFloat(flon, 5);
 }
 
-
-
-
-
+// not sure why this needs to be here, but removing it breaks the code...
 void printFloat(double f, int digits = 2);
 
-
-
+// print float to serial
 void printFloat(double number, int digits)
 {
   // Handle negative numbers
@@ -109,14 +98,16 @@ void printFloat(double number, int digits)
   }
 }
 
+// for converting between degrees and radians
+const double p i= 3.14159265358979;
 
-const double pi= 3.14159265358979;
-double distance(double lat1, double long1, double lat2, double long2) { //find distance between two lat-long pairs
-  //an application of the Haversine formula
+// find distance between two lat-long pairs
+double distance(double lat1, double long1, double lat2, double long2) {
+  // an application of the Haversine formula
   
-  //returns answer in metres
+  // returns answer in metres
   
-  double R = 6371e3; //radius of the Earth, in metres (change to km for answer to be in km, etc)
+  double R = 6371e3; // radius of the Earth, in metres (change to km for answer to be in km, etc)
 
   double lat1Radians = toRadians(lat1);
   double lat2Radians = toRadians(lat2);
@@ -134,27 +125,9 @@ double distance(double lat1, double long1, double lat2, double long2) { //find d
   return d;
 }
 
-double distanceAlternative(double lat1, double long1, double lat2, double long2) { //find distance between two lat-long pairs (alternate, easier method)
-  //an application of the cosine rule
-  
-  //returns answer in metres
-  
-  double R = 6371e3; //radius of the Earth, in metres (change to km for answer to be in km, etc)
-
-  double lat1Radians = toRadians(lat1);
-  double lat2Radians = toRadians(lat2);
-  
-  double deltaLong = toRadians(long2-long1);
-  
-  double d = acos( sin(lat1Radians) * sin(lat2Radians) + cos(lat1Radians) * cos(lat2Radians) * cos(deltaLong) ) * R;
-
-  return d;
-}
-
-double bearing(float lat1, double long1, double lat2, double long2) { //find bearing between two lat-long pairs
-  //untested so far...
-  
-  //returns answer in radians (can easily be converted to degrees if necessary)
+// find bearing between two lat-long pairs
+double bearing(float lat1, double long1, double lat2, double long2) {
+  // returns answer in radians (can easily be converted to degrees if necessary)
   
   double y = sin(long2-long1) * cos(lat2);
   double x = cos(lat1)*sin(lat2) -
@@ -165,10 +138,12 @@ double bearing(float lat1, double long1, double lat2, double long2) { //find bea
   return toDegrees(bearing);
 }
 
-double toRadians(double degrees) { //convert degrees to radians (cpp functions accept radians only)
+// convert degrees to radians (cpp trig functions use radians)
+double toRadians(double degrees) {
   return degrees * pi / 180;
 }
 
+// convert radians to degrees
 double toDegrees(double radian) {
   double degree = radian / pi * 180;
   if ( degree < 0) {
@@ -177,13 +152,14 @@ double toDegrees(double radian) {
   return degree;
 }
 
-
+// move car forward a certain amount of cm
 void moveForward(int distance) { // distance is in cm
   digitalWrite(13, HIGH);
   delay(distance * 9.32);
   digitalWrite(13, LOW);
 }
 
+// turn car right a certian amount of degrees
 void turnRight(int rotation) { // rotation is in degrees
   digitalWrite(12, HIGH);
   digitalWrite(13, HIGH);
@@ -192,128 +168,116 @@ void turnRight(int rotation) { // rotation is in degrees
   digitalWrite(13, LOW);
 }
 
+float readMagnetometerHeading() {
+  int x, y, z;
+
+  if(!mag.isCalibrated()) //If we're not calibrated
+  {
+    if(!mag.isCalibrating()) //And we're not currently calibrating
+    {
+      Serial.println("Entering calibration mode");
+      mag.enterCalMode(); //This sets the output data rate to the highest possible and puts the mag sensor in active mode
+    }
+    else
+    {
+      //Must call every loop while calibrating to collect calibration data
+      //This will automatically exit calibration
+      //You can terminate calibration early by calling mag.exitCalMode();
+      mag.calibrate(); 
+    }
+  }
+  else
+  {
+    Serial.println("Calibrated!");
+  }
+  
+  return mag.readHeading();
+}
+
 void loop() // run over and over
 {
-  /*
-  longLat();
-  Serial.println(lon);
-  Serial.println(lat);
-  if (lat != 0) {
-    //turnRight(bearing(lat, lon, destinationLat, destinationLon));
-    //moveForward(distance(lat, lon, destinationLat, destinationLon));
-  }
-
+  // turn on motors for magnetometer calibration
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
   
-  delay(1000);*/
+  float magHeading = readMagnetometerHeading();
 
+  if (magHeading !== 0) { // check that the magnetometer is calibrated
 
-
-  
- /* bool newdata = false;
-
-  unsigned long start = millis();
-  // Every 5 seconds we print an update
-  
-  while (millis() - start < 5000) 
-  {
-    if (mySerial.available()) 
+    // turn off motors from calibration
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
     
+    bool newdata = false;
+    unsigned long start = millis();
+    while (millis() - start < 5000)  // every 5 seconds we print an update
     {
-      Serial.println("Calculating current GPS position...");
-      char c = mySerial.read();
-      //Serial.print(c);  // uncomment to see raw GPS data
-      if (gps.encode(c)) 
+      if (mySerial.available()) 
+      
       {
-        Serial.println("GPS data found.");
-        newdata = true;
-        break;  // uncomment to print new data immediately!
+        Serial.println("Searching for GPS data...");
+        char c = mySerial.read();
+        //Serial.print(c);  // uncomment to see raw GPS data
+        if (gps.encode(c)) 
+        {
+          Serial.println("GPS data found.");
+          newdata = true;
+          break;  // uncomment to print new data immediately!
+        }
       }
     }
-  }
-  
-  if (newdata) 
-  {
-    float lat = latitude(gps);
-    float lon = longitude(gps);
-
-    destinationLat = lat + 0.00001;
-    destinationLon = lon + 0.00001;
-
-    Serial.println("--- New data ---");
     
-    Serial.println("Current latitude: ");
-    printFloat(lat, 6);
-    Serial.println("Current longitude: ");
-    printFloat(lon, 6);
-
-    Serial.println("Destination latitude: ");
-    printFloat(destinationLat, 6);
-    Serial.println("Destination longitude: ");
-    printFloat(destinationLon, 6);
-
-    Serial.println("Bearing: ");
-    printFloat(bearing(lat, lon, destinationLat, destinationLon));
-    Serial.println("Distance: ");
-    printFloat(distance(lat, lon, destinationLat, destinationLon));
-  }*/
-
-  bool newdata = false;
-  unsigned long start = millis();
-  while (millis() - start < 5000) 
-  {
-    if (mySerial.available()) 
-    
+    if (newdata) 
     {
-      //Serial.println("Searching for GPS data...");
-      char c = mySerial.read();
-      //Serial.print(c);  // uncomment to see raw GPS data
-      if (gps.encode(c)) 
-      {
-        Serial.println("GPS data found.");
-        newdata = true;
-        break;  // uncomment to print new data immediately!
+      float lat = latitude(gps);
+      float lon = longitude(gps);
+
+      // face approximately north (perhaps should be less approximate in the future
+      digitalWrite(12, HIGH);
+      digitalWrite(13, HIGH);
+      while(magHeading > 15 || magHeading < -15) {
+        // not facing north; keep moving and get another reading
+        magHeading = readMagnetometerHeading();
       }
-    }
-  }
+      digitalWrite(12, LOW);
+      digitalWrite(13, LOW);
+
+      // test code
+      destinationLat = lat + 0;
+      destinationLon = lon + 100;
   
-  if (newdata) 
-  {
-    float lat = latitude(gps);
-    float lon = longitude(gps);
+      Serial.println("--- New data ---");
+      
+      Serial.println("");
+      Serial.println("Current latitude: ");
+      printFloat(lat, 6);
+      Serial.println("");
+      Serial.println("Current longitude: ");
+      printFloat(lon, 6);
+  
+      Serial.println("");
+      Serial.println("Destination latitude: ");
+      printFloat(destinationLat, 6);
+      Serial.println("");
+      Serial.println("Destination longitude: ");
+      printFloat(destinationLon, 6);
+  
+      Serial.println("");
+      Serial.println("Bearing: ");
+      printFloat(bearing(lat, lon, destinationLat, destinationLon));
+      Serial.println("");
+      Serial.println("Distance: ");
+      printFloat(distance(lat, lon, destinationLat, destinationLon));
+      Serial.println("");
 
-    destinationLat = lat + 0;
-    destinationLon = lon + 100;
-
-    Serial.println("--- New data ---");
-    
-    Serial.println("");
-    Serial.println("Current latitude: ");
-    printFloat(lat, 6);
-    Serial.println("");
-    Serial.println("Current longitude: ");
-    printFloat(lon, 6);
-
-    Serial.println("");
-    Serial.println("Destination latitude: ");
-    printFloat(destinationLat, 6);
-    Serial.println("");
-    Serial.println("Destination longitude: ");
-    printFloat(destinationLon, 6);
-
-    Serial.println("");
-    Serial.println("Bearing: ");
-    printFloat(bearing(lat, lon, destinationLat, destinationLon));
-    Serial.println("");
-    Serial.println("Distance: ");
-    printFloat(distance(lat, lon, destinationLat, destinationLon));
-    Serial.println("");
-    
-    turnRight(bearing(lat, lon, destinationLat, destinationLon) * 5);
+      // move towards destination
+      turnRight(bearing(lat, lon, destinationLat, destinationLon) * 5);
+      delay(1000);
+      moveForward(distance(lat, lon, destinationLat, destinationLon) * 100);
+  
+      Serial.println("Destination reached.");
+    }
     delay(1000);
-    moveForward(distance(lat, lon, destinationLat, destinationLon) * 100);
-
-    Serial.println("Destination reached.");
+    Serial.println(" Recalculating position...");
   }
-  delay(1000);
-  Serial.println(" Recalculating position...");
 }
