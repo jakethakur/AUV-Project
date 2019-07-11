35d98f313fb7d8e81f941242969a7e603883da43
Jake Thakur <jakethakur@pop-os.localdomain>
2019-07-11 13:21:35 +0100

    magnetometer testing code

---
+++ magnetometer/magnetometer/magnetometer.ino
@@ -0,0 +1,93 @@
+
+
+
+#include <SparkFun_MAG3110.h>
+
+MAG3110 mag = MAG3110(); //Instantiate MAG3110
+
+void calibrateMag() {
+  if(!mag.isCalibrated()) //If we're not calibrated
+  {
+    if(!mag.isCalibrating()) //And we're not currently calibrating
+    {
+      Serial.println("Entering calibration mode");
+      mag.enterCalMode(); //This sets the output data rate to the highest possible and puts the mag sensor in active mode
+    }
+    else
+    {
+      //Must call every loop while calibrating to collect calibration data
+      //This will automatically exit calibration
+      //You can terminate calibration early by calling mag.exitCalMode();
+      mag.calibrate(); 
+    }
+  }
+  else
+  {
+    Serial.println("Calibrated!");
+  }
+}
+
+
+
+
+void printFloat(double f, int digits = 2);
+
+void printFloat(double number, int digits)
+{
+  // Handle negative numbers
+  if (number < 0.0) 
+  {
+     Serial.print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  Serial.print(int_part);
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0)
+    Serial.print("."); 
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0) 
+  {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    Serial.print(toPrint);
+    remainder -= toPrint;
+  }
+}
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  mag.initialize(); //Initializes the mag sensor
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(!mag.isCalibrated())
+  {
+    calibrateMag();
+  }
+  else {
+      float bearingValue = mag.readHeading();
+      printFloat(bearingValue);
+      Serial.println("---");
+  }
+}
